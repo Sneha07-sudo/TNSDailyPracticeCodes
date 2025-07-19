8ea7abb03f18fd1549d245f23d66d482ee359c93
Snehal Biradar <snehalbiradar07@gmail.com>
2025-07-19 16:53:06 +0530

    Add files via upload

---
+++ DaySix/usingfinal/FinalMethodClass.java
@@ -0,0 +1,16 @@
+package DaySix.usingfinal;
+
+public final class FinalMethodClass {
+	
+	final int a = 50;
+	
+	final void show()
+	{
+		System.out.println("Value of a: "+a);
+	}
+
+//	final void show()
+//	{
+//		System.out.println("show Method");
+//	}
+}
