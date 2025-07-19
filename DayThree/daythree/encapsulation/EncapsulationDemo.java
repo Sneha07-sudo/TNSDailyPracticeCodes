b22dc619c7264dd1fb903c0581bf654b52082c5e
Snehal Biradar <snehalbiradar07@gmail.com>
2025-07-19 16:24:26 +0530

    Add files via upload

---
+++ DayThree/daythree/encapsulation/EncapsulationDemo.java
@@ -0,0 +1,20 @@
+package DayThree.daythree.encapsulation;
+
+public class EncapsulationDemo {
+
+	public static void main(String[] args) {
+
+		
+		//object creation
+		
+		OopsConceptDemo obj = new OopsConceptDemo();
+		
+		obj.setSerialNum(101);
+		obj.setName("Pooja");
+		obj.setAge(21);
+		
+		System.out.println(obj);
+		
+	}
+
+}
---
+++ DayThree/daythreeconstructor/ConstructorStudent.java
@@ -0,0 +1,12 @@
+package DayThree.daythreeconstructor;
+
+public class ConstructorStudent extends Student {
+
+	public static void main(String[] args) {
+		
+//		Student obj = new Student();
+		
+		ConstructorStudent obj = new ConstructorStudent();
+	}
+
+}
